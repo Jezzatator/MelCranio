ce21bf39a961c39b9fa6291466a82945a02a4706
Jezzatator <jeremie.patot@protonmail.ch>
2023-12-09 15:27:40 +0100

    suppression de Supabase dans layout pour Vercel

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,6 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import NavBar from "./components/NavBar";
 import Footer from "./components/Footer";
-import Admin from "./admin/page";
 
 const inter = Inter({ subsets: ["latin"] });
 
