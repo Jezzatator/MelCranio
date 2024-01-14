47dd304d219403611ad7c04db364b02a999140f5
Jezzatator <jeremie.patot@protonmail.ch>
2024-01-14 14:58:45 +0100

    comment out fonction fetch

--- app/page.tsx
+++ app/page.tsx
@@ -4,37 +4,37 @@ import "./frontpage.css";
 import Lieux from "./places/page";
 import { NextApiResponse } from "next";
 
-async function getData(): Promise<any> {
-  const apiKey = "Bearer " + process.env.FULL_ACCESS_KEY;
+// async function getData(): Promise<any> {
+//   const apiKey = "Bearer " + process.env.FULL_ACCESS_KEY;
 
-  try {
-    const res: Response = await fetch(
-      "https://mel-cranio-jeremiepatot.koyeb.app/api/homes?locale=ca",
-      {
-        method: "GET",
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: apiKey,
-        },
-      }
-    );
+//   try {
+//     const res: Response = await fetch(
+//       "https://mel-cranio-jeremiepatot.koyeb.app/api/homes?locale=ca",
+//       {
+//         method: "GET",
+//         headers: {
+//           "Content-Type": "application/json",
+//           Authorization: apiKey,
+//         },
+//       }
+//     );
 
-    if (!res.ok) {
-      // This will activate the closest `error.js` Error Boundary
-      throw new Error("Failed to fetch data");
-    }
+//     if (!res.ok) {
+//       // This will activate the closest `error.js` Error Boundary
+//       throw new Error("Failed to fetch data");
+//     }
 
-    const data = await res.json();
-    return data;
-  } catch (error) {
-    console.error("Error fetching data:", error);
-    throw error;
-  }
-}
+//     const data = await res.json();
+//     return data;
+//   } catch (error) {
+//     console.error("Error fetching data:", error);
+//     throw error;
+//   }
+// }
 
 export default async function Home() {
-  const data = await getData();
-  console.log(data.data[0].attributes.subNameFirstCard);
+  // const data = await getData();
+  // console.log(data.data[0].attributes.subNameFirstCard);
 
   return (
     <div className="px-20 py-80 text-amber-950 mt-20 ">
@@ -47,7 +47,8 @@ export default async function Home() {
             Mélissa Patot
           </h1>
           <h2 className="text-xl pl-2 font-alegreyaSans">
-            {data.data[0].attributes.subNameFirstCard}
+            Kinésithérapeute spécialisé en thérapie craniosacrale
+            {/* {data.data[0].attributes.subNameFirstCard} */}
           </h2>
         </div>
         <div
