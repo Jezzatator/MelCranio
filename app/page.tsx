667485b101765fcca0a54890d95b6bccbf912bd1
Jezzatator <jeremie.patot@protonmail.ch>
2024-02-01 16:16:51 +0100

    fix env vercel

--- app/page.tsx
+++ app/page.tsx
@@ -54,11 +54,8 @@ async function getData() {
       },
     }
   );
-  // The return value is *not* serialized
-  // You can return Date, Map, Set, etc.
 
   if (!res.ok) {
-    // This will activate the closest `error.js` Error Boundary
     throw new Error("Failed to fetch data. Status: " + res.status);
   }
 
