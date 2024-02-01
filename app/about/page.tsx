fa19e44919120ca7d7532e7fcef6ec747c5115d7
Jezzatator <jeremie.patot@protonmail.ch>
2024-02-01 14:41:00 +0100

    debut ajout strapi

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,15 +1,21 @@
 import React from "react";
+import Timeline from "../components/TimeLine";
+// import {
+//   VerticalTimeline,
+//   VerticalTimelineElement,
+// } from "react-vertical-timeline-component";
+// import "react-vertical-timeline-component/style.min.css";
 
 const About = () => {
   return (
     <div className="pt-20">
       {/* <div className="flex flex-row m-5 pb-20 pt-20 px-20 py-20 text-amber-950 mt-10"></div> */}
       <div className="text-left flex flex-row Deuxieme Card Parcourd px-20 py-20">
-        <div className=" px-5 pr-40 basis-2/3">
-          <h2 className="text-2xl p-4 md:p-0 text-center md:text-left font-bold font-alegreyaSans">
+        <div className=" px-5 basis-2/3">
+          <h2 className="text-2xl p-4 md:p-0 text-center md:text-left font-bold font-alegreyaSans mb-10">
             À Propos de Moi
           </h2>
-          <p className="text-center md:text-left mb-4 pt-5 pl-10 leading-8 text-lg">
+          <p className="text-center md:text-left mb-5 pt-5 pl-10 leading-loose text-lg">
             Après avoir terminé mes études de physiothérapie à Gérone (Espagne),
             j&apos;ai découvert que dans les études de physiothérapie, nous ne
             sommes pas préparés de manière optimale au stress mental ou
@@ -17,30 +23,32 @@ const About = () => {
             thérapie cranio-sacré car cette méthode se concentre sur le lien
             direct entre l’état émotionnel et les symptômes physiques.
           </p>
-          <p className="text-center md:text-left mb-4 pt-5 pl-10 leading-8 text-lg">
+          <p className="text-center md:text-left mb-5 pt-5 pl-10 leading-loose text-lg">
             J&apos;ai suivi le cours de base cranio-sacré à l&apos;Académie
             internationale cranio-sacrée ICSA à Salzbourg (Autriche). Je me
             spécialise dans la thérapie viscérale cranio-sacré, qui est très
             utile pour les patients souffrant de maux de dos chroniques et peu
             clairs et qui peut très bien être intégrée à la physiothérapie.
           </p>
-          <p className="text-center md:text-left mb-4 pt-5 pl-10 leading-8 text-lg">
-            Lorsque j&apos;ai émigré en Suisse il y a 3 ans, il était clair pour
-            moi que je devais obtenir le certificat industriel Oda KT.
+          <p className="text-center md:text-left mb-5 pt-5 pl-10 leading-loose text-lg">
+            Lorsque j&apos;ai émigré en Suisse en Juillet 2020, il était clair
+            pour moi que je devais obtenir le certificat industriel Oda KT.
             C&apos;est pourquoi j&apos;ai pu approfondir mes connaissances sur
             cette méthode. J&apos;ai approfondi, notamment dans des techniques
             spécifiques, telles que le nerf crânien, le soutien pendant la
             grossesse et l&apos;engagement social au Colorado Institute et à
             l&apos;Apamed (Zurich/Jona).
           </p>
-          <p className="text-center md:text-left mb-4 pt-5 pl-10 leading-8 text-lg">
+          <p className="text-center md:text-left mb-5 pt-5 pl-10 leading-loose text-lg">
             Ces techniques apprises élargissent mes compétences
             professionnelles, par exemple en matière de migraines,
             d&apos;acouphènes, d&apos;épuisement professionnel, de TDAH et bien
             plus encore.
           </p>
         </div>
-        <div className="basis1/3 p-5">IMAGE</div>
+        <div className="p-5 basis-2/3">
+          <Timeline />
+        </div>
       </div>
     </div>
   );
--- app/craniosacral/page.tsx
+++ app/craniosacral/page.tsx
@@ -1,7 +1,38 @@
 import Image from "next/image";
 import React from "react";
+import { NextApiResponse } from "next";
 
-const Craniosacral = () => {
+async function getData(): Promise<any> {
+  const apiKey = "Bearer " + process.env.READ_ONLY_KEY;
+
+  try {
+    const res: Response = await fetch(
+      "https://mel-cranio-jeremiepatot.koyeb.app/api/la-cranios?locale=fr",
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: apiKey,
+        },
+      }
+    );
+
+    if (!res.ok) {
+      throw new Error("Failed to fetch data");
+    }
+
+    const data = await res.json();
+    return data;
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    throw error;
+  }
+}
+
+export default async function Craniosacral() {
+  const data = await getData();
+  const lacranioData = data.data[0].attributes;
+  console.log(data.data[0].attributes.TitleCranio);
   return (
     <div className=" text-amber-950 pt-20">
       {/* Craniosacral introduction */}
@@ -9,24 +40,28 @@ const Craniosacral = () => {
       <div className="flex flex-row text-left  pb-20 px-20 py-20">
         <div className="px-5 pr-40 basis-1/2">
           <h2 className="text-2xl p-4 md:p-0 text-center md:text-left font-bold font-alegreyaSans">
-            Qu&apos;est-ce que la Thérapie Craniosacrale?
+            {/* Qu&apos;est-ce que la Thérapie Craniosacrale? */}
+            {lacranioData.TitleCranio}
           </h2>
           <p className="text-center md:text-left mb-4 pt-5 pl-10 leading-8 text-lg">
-            La thérapie craniosacrale libère les tensions du corps pour
+            {/* La thérapie craniosacrale libère les tensions du corps pour
             favoriser la détente et la guérison. Elle explore la connexion
-            subtile entre le crâne, la colonne vertébrale, et le sacrum.
+            subtile entre le crâne, la colonne vertébrale, et le sacrum. */}
+            {lacranioData.sousTextTitle[0].children[0].text}
           </p>
 
           {/* System Explanation */}
 
           <h2 className="text-l p-4 text-centermd:text-left font-semibold pt-20">
-            Comprendre le Système Craniosacral
+            {/* Comprendre le Système Craniosacral */}
+            {lacranioData.titleH2}
           </h2>
           <p className="text-center md:text-left mb-4 pt-5 pl-10 leading-8 text-lg">
-            Découvrez le système craniosacral, un réseau délicat composé du
+            {/* Découvrez le système craniosacral, un réseau délicat composé du
             crâne, de la colonne vertébrale et du sacrum. Ce système, abritant
             le fluide céphalorachidien essentiel, joue un rôle crucial dans le
-            bon fonctionnement de votre corps.
+            bon fonctionnement de votre corps. */}
+            {lacranioData.contentTH2[0].children[0].text}
           </p>
         </div>
 
@@ -34,24 +69,28 @@ const Craniosacral = () => {
           {/* Session Details */}
 
           <h2 className="text-l p-4 text-centermd:text-left font-semibold">
-            Déroulement d&apos;une Séance de Thérapie Craniosacrale
+            {/* Déroulement d&apos;une Séance de Thérapie Craniosacrale */}
+            {lacranioData.titleH22}
           </h2>
           <p className="text-center md:text-left mb-4 pt-5 pl-10 leading-8 text-lg">
-            Pendant une séance de thérapie craniosacrale, le ou la thérapeute
+            {/* Pendant une séance de thérapie craniosacrale, le ou la thérapeute
             utilise des techniques douces, non invasives, pour évaluer et
-            améliorer le mouvement du liquide céphalorachidien.
+            améliorer le mouvement du liquide céphalorachidien. */}
+            {lacranioData.contentTH22[0].children[0].text}
           </p>
           <p className="text-center md:text-left mb-4 pt-5 pl-10 leading-8 text-lg">
-            Cette approche délicate permet de libérer les blocages, réduire les
+            {/* Cette approche délicate permet de libérer les blocages, réduire les
             tensions et restaurer la mobilité naturelle des structures
-            crâniennes et vertébrales.
+            crâniennes et vertébrales. */}
+            {lacranioData.contentTH22[1].children[0].text}
           </p>
           <p className="text-center md:text-left mb-4 pt-5 pl-10 leading-8 text-lg">
-            Au fil de la séance, le patient ressent souvent une profonde
+            {/* Au fil de la séance, le patient ressent souvent une profonde
             relaxation, ce qui favorise la réduction du stress et de
             l&apos;anxiété. La thérapie craniosacrale peut également aider à
             soulager les maux de tête, les douleurs physiques, et à améliorer la
-            qualité du sommeil.
+            qualité du sommeil. */}
+            {lacranioData.contentTH22[2].children[0].text}
           </p>
         </div>
       </div>
@@ -69,19 +108,26 @@ const Craniosacral = () => {
           >
             <ul className="text-left space-y-3 p-4 z-30">
               <h3 className="text-2xl py-8 font-alegreyaSans">
-                Bénéfices de la Thérapie Craniosacrale
+                {/* Bénéfices de la Thérapie Craniosacrale */}
+                {lacranioData.beneficesTitle}
               </h3>
               <li className="px-2">
                 <p className="font-semibold">
-                  Réduction du stress et de l&apos;anxiété :
+                  {/* Réduction du stress et de l&apos;anxiété : */}
+                  {lacranioData.beneficesContent[0].children[0].text}
                 </p>
-                En favorisant la relaxation, la thérapie craniosacrale contribue
-                à réduire les niveaux de stress et d&apos;anxiété.
+                {/* En favorisant la relaxation, la thérapie craniosacrale contribue
+                à réduire les niveaux de stress et d&apos;anxiété. */}
+                {lacranioData.beneficesContent[1].children[0].text}
               </li>
               <li className="px-2">
-                <p className="font-semibold"> Amélioration du sommeil : </p>La
-                détente induite par la thérapie craniosacrale peut favoriser un
-                sommeil plus profond et réparateur.
+                <p className="font-semibold">
+                  {/* Amélioration du sommeil : */}
+                  {lacranioData.beneficesContent[2].children[0].text}
+                </p>
+                {/* La détente induite par la thérapie craniosacrale peut favoriser un
+                sommeil plus profond et réparateur. */}
+                {lacranioData.beneficesContent[3].children[0].text}
               </li>
               <li className="px-2">
                 <p className="font-semibold">
@@ -111,6 +157,4 @@ const Craniosacral = () => {
       </div>
     </div>
   );
-};
-
-export default Craniosacral;
+}
